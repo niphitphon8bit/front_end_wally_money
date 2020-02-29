cd316c609afc5a8e6941b59b450490f74519ef0a
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-02-29 19:26:48 +0700

    update tab2 to main_menu edit app.routing.module Signed-off-by: niphitphon8bit <niphitphon_nt@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
   }
 ];
 @NgModule({
